e7987716405253ecb880f0ac08d3d72c70fbb25e
Lil_Ken <lil_ken@163.com>
2025-01-15 22:45:04 +0800

    amend regpointnet and drivaernetplusplus.py

--- ppsci/arch/regpointnet.py
+++ ppsci/arch/regpointnet.py
@@ -119,135 +119,136 @@ class RegPointNet(paddle.nn.Layer):
         forward(x): Forward pass through the network.
     """
 
-
-def __init__(
-    self,
-    input_keys: Tuple[str, ...],
-    label_keys: Tuple[str, ...],
-    weight_keys: Tuple[str, ...],
-    args,
-):
-    super(RegPointNet, self).__init__()
-    self.input_keys = input_keys
-    self.label_keys = label_keys
-    self.weight_keys = weight_keys
-    self.args = args
-    self.conv1 = paddle.nn.Conv1D(
-        in_channels=3, out_channels=512, kernel_size=1, bias_attr=False
-    )
-    self.conv2 = paddle.nn.Conv1D(
-        in_channels=512, out_channels=1024, kernel_size=1, bias_attr=False
-    )
-    self.conv3 = paddle.nn.Conv1D(
-        in_channels=1024, out_channels=1024, kernel_size=1, bias_attr=False
-    )
-    self.conv4 = paddle.nn.Conv1D(
-        in_channels=1024, out_channels=1024, kernel_size=1, bias_attr=False
-    )
-    self.conv5 = paddle.nn.Conv1D(
-        in_channels=1024, out_channels=1024, kernel_size=1, bias_attr=False
-    )
-    self.conv6 = paddle.nn.Conv1D(
-        in_channels=1024,
-        out_channels=args["emb_dims"],
-        kernel_size=1,
-        bias_attr=False,
-    )
-    self.bn1 = paddle.nn.BatchNorm1D(num_features=512)
-    self.bn2 = paddle.nn.BatchNorm1D(num_features=1024)
-    self.bn3 = paddle.nn.BatchNorm1D(num_features=1024)
-    self.bn4 = paddle.nn.BatchNorm1D(num_features=1024)
-    self.bn5 = paddle.nn.BatchNorm1D(num_features=1024)
-    self.bn6 = paddle.nn.BatchNorm1D(num_features=args["emb_dims"])
-    self.dropout_conv = paddle.nn.Dropout(p=args["dropout"])
-    self.dropout_linear = paddle.nn.Dropout(p=args["dropout"])
-    self.conv_shortcut = paddle.nn.Conv1D(
-        in_channels=3, out_channels=args["emb_dims"], kernel_size=1, bias_attr=False
-    )
-    self.bn_shortcut = paddle.nn.BatchNorm1D(num_features=args["emb_dims"])
-    self.linear1 = paddle.nn.Linear(
-        in_features=args["emb_dims"], out_features=512, bias_attr=False
-    )
-    self.bn7 = paddle.nn.BatchNorm1D(num_features=512)
-    self.linear2 = paddle.nn.Linear(in_features=512, out_features=256, bias_attr=False)
-    self.bn8 = paddle.nn.BatchNorm1D(num_features=256)
-    self.linear3 = paddle.nn.Linear(in_features=256, out_features=128)
-    self.bn9 = paddle.nn.BatchNorm1D(num_features=128)
-    self.linear4 = paddle.nn.Linear(in_features=128, out_features=64)
-    self.bn10 = paddle.nn.BatchNorm1D(num_features=64)
-    self.final_linear = paddle.nn.Linear(in_features=64, out_features=1)
-
-
-def min_max_normalize(self, data: paddle.Tensor) -> paddle.Tensor:
-    """
-    Normalizes the data to the range [0, 1] based on min and max values.
-    """
-    min_vals = data.min(axis=0, keepdim=True)
-    max_vals = data.max(axis=0, keepdim=True)
-    normalized_data = (data - min_vals) / (max_vals - min_vals)
-    return normalized_data
-
-
-def forward(self, x):
-    """
-    Forward pass of the network.
-
-    Args:
-        x (Tensor): Input tensor of shape (batch_size, 3, num_points).
-
-    Returns:
-        paddle.Tensor: Output tensor of the predicted scalar value.
-    """
-
-    x = x[self.input_keys[0]]
-    bs = x.shape[0]
-
-    # Ensure x is in the shape (bs, C, L)
-    if len(x.shape) != 3:
-        raise ValueError(
-            f"Input tensor should have shape (batch_size, C, L), but got {x.shape}"
+    def __init__(
+        self,
+        input_keys: Tuple[str, ...],
+        label_keys: Tuple[str, ...],
+        weight_keys: Tuple[str, ...],
+        args,
+    ):
+        super().__init__()
+        self.input_keys = input_keys
+        self.label_keys = label_keys
+        self.weight_keys = weight_keys
+        self.args = args
+        self.conv1 = paddle.nn.Conv1D(
+            in_channels=3, out_channels=512, kernel_size=1, bias_attr=False
         )
-
-    # Initialize an empty list to store the processed samples
-    processed_samples = []
-
-    # Apply data augmentation and normalization for each sample in the batch
-    augmentation = DataAugmentation()
-    for i in range(bs):
-        sample = x[i].numpy()  # Convert to numpy array for data augmentation
-        sample = augmentation.translate_pointcloud(sample)
-        sample = augmentation.jitter_pointcloud(sample)
-        sample = self.min_max_normalize(sample)
-        processed_samples.append(sample)
-
-    # Stack the processed samples back into a batch tensor
-    x_processed = paddle.to_tensor(np.stack(processed_samples, axis=0))
-
-    # Ensure the processed tensor has the same shape as the original input
-    if x_processed.shape != x.shape:
-        raise ValueError(
-            f"Processed tensor shape {x_processed.shape} does not match original input shape {x.shape}"
+        self.conv2 = paddle.nn.Conv1D(
+            in_channels=512, out_channels=1024, kernel_size=1, bias_attr=False
         )
+        self.conv3 = paddle.nn.Conv1D(
+            in_channels=1024, out_channels=1024, kernel_size=1, bias_attr=False
+        )
+        self.conv4 = paddle.nn.Conv1D(
+            in_channels=1024, out_channels=1024, kernel_size=1, bias_attr=False
+        )
+        self.conv5 = paddle.nn.Conv1D(
+            in_channels=1024, out_channels=1024, kernel_size=1, bias_attr=False
+        )
+        self.conv6 = paddle.nn.Conv1D(
+            in_channels=1024,
+            out_channels=args["emb_dims"],
+            kernel_size=1,
+            bias_attr=False,
+        )
+        self.bn1 = paddle.nn.BatchNorm1D(num_features=512)
+        self.bn2 = paddle.nn.BatchNorm1D(num_features=1024)
+        self.bn3 = paddle.nn.BatchNorm1D(num_features=1024)
+        self.bn4 = paddle.nn.BatchNorm1D(num_features=1024)
+        self.bn5 = paddle.nn.BatchNorm1D(num_features=1024)
+        self.bn6 = paddle.nn.BatchNorm1D(num_features=args["emb_dims"])
+        self.dropout_conv = paddle.nn.Dropout(p=args["dropout"])
+        self.dropout_linear = paddle.nn.Dropout(p=args["dropout"])
+        self.conv_shortcut = paddle.nn.Conv1D(
+            in_channels=3, out_channels=args["emb_dims"], kernel_size=1, bias_attr=False
+        )
+        self.bn_shortcut = paddle.nn.BatchNorm1D(num_features=args["emb_dims"])
+        self.linear1 = paddle.nn.Linear(
+            in_features=args["emb_dims"], out_features=512, bias_attr=False
+        )
+        self.bn7 = paddle.nn.BatchNorm1D(num_features=512)
+        self.linear2 = paddle.nn.Linear(
+            in_features=512, out_features=256, bias_attr=False
+        )
+        self.bn8 = paddle.nn.BatchNorm1D(num_features=256)
+        self.linear3 = paddle.nn.Linear(in_features=256, out_features=128)
+        self.bn9 = paddle.nn.BatchNorm1D(num_features=128)
+        self.linear4 = paddle.nn.Linear(in_features=128, out_features=64)
+        self.bn10 = paddle.nn.BatchNorm1D(num_features=64)
+        self.final_linear = paddle.nn.Linear(in_features=64, out_features=1)
 
-    x_processed = x_processed.transpose(perm=[0, 2, 1])
+    def min_max_normalize(self, data: paddle.Tensor) -> paddle.Tensor:
+        """
+        Normalizes the data to the range [0, 1] based on min and max values.
+        """
+        min_vals = data.min(axis=0, keepdim=True)
+        max_vals = data.max(axis=0, keepdim=True)
+        normalized_data = (data - min_vals) / (max_vals - min_vals)
+        return normalized_data
 
-    shortcut = self.bn_shortcut(self.conv_shortcut(x_processed))
-    x = paddle.nn.functional.relu(x=self.bn1(self.conv1(x_processed)))
-    x = self.dropout_conv(x)
-    x = paddle.nn.functional.relu(x=self.bn2(self.conv2(x)))
-    x = self.dropout_conv(x)
-    x = paddle.nn.functional.relu(x=self.bn3(self.conv3(x)))
-    x = self.dropout_conv(x)
-    x = paddle.nn.functional.relu(x=self.bn4(self.conv4(x)))
-    x = self.dropout_conv(x)
-    x = paddle.nn.functional.relu(x=self.bn5(self.conv5(x)))
-    x = self.dropout_conv(x)
-    x = paddle.nn.functional.relu(x=self.bn6(self.conv6(x)))
-    x = x + shortcut
-    x = paddle.nn.functional.adaptive_max_pool1d(x=x, output_size=1).squeeze(axis=-1)
-    x = paddle.nn.functional.relu(x=self.bn7(self.linear1(x)))
-    x = paddle.nn.functional.relu(x=self.bn8(self.linear2(x)))
-    x = paddle.nn.functional.relu(x=self.bn9(self.linear3(x)))
-    x = paddle.nn.functional.relu(x=self.bn10(self.linear4(x)))
-    x = self.final_linear(x)
-    return {self.label_keys[0]: x}
+    def forward(self, x):
+        """
+        Forward pass of the network.
+
+        Args:
+            x (Tensor): Input tensor of shape (batch_size, 3, num_points).
+
+        Returns:
+            paddle.Tensor: Output tensor of the predicted scalar value.
+        """
+
+        x = x[self.input_keys[0]]
+        bs = x.shape[0]
+
+        # Ensure x is in the shape (bs, C, L)
+        if len(x.shape) != 3:
+            raise ValueError(
+                f"Input tensor should have shape (batch_size, C, L), but got {x.shape}"
+            )
+
+        # Initialize an empty list to store the processed samples
+        processed_samples = []
+
+        # Apply data augmentation and normalization for each sample in the batch
+        augmentation = DataAugmentation()
+        for i in range(bs):
+            sample = x[i].numpy()  # Convert to numpy array for data augmentation
+            sample = augmentation.translate_pointcloud(sample)
+            sample = augmentation.jitter_pointcloud(sample)
+            sample = self.min_max_normalize(sample)
+            processed_samples.append(sample)
+
+        # Stack the processed samples back into a batch tensor
+        x_processed = paddle.to_tensor(np.stack(processed_samples, axis=0))
+
+        # Ensure the processed tensor has the same shape as the original input
+        if x_processed.shape != x.shape:
+            raise ValueError(
+                f"Processed tensor shape {x_processed.shape} does not match original input shape {x.shape}"
+            )
+
+        x_processed = x_processed.transpose(perm=[0, 2, 1])
+
+        shortcut = self.bn_shortcut(self.conv_shortcut(x_processed))
+        x = paddle.nn.functional.relu(x=self.bn1(self.conv1(x_processed)))
+        x = self.dropout_conv(x)
+        x = paddle.nn.functional.relu(x=self.bn2(self.conv2(x)))
+        x = self.dropout_conv(x)
+        x = paddle.nn.functional.relu(x=self.bn3(self.conv3(x)))
+        x = self.dropout_conv(x)
+        x = paddle.nn.functional.relu(x=self.bn4(self.conv4(x)))
+        x = self.dropout_conv(x)
+        x = paddle.nn.functional.relu(x=self.bn5(self.conv5(x)))
+        x = self.dropout_conv(x)
+        x = paddle.nn.functional.relu(x=self.bn6(self.conv6(x)))
+        x = x + shortcut
+        x = paddle.nn.functional.adaptive_max_pool1d(x=x, output_size=1).squeeze(
+            axis=-1
+        )
+        x = paddle.nn.functional.relu(x=self.bn7(self.linear1(x)))
+        x = paddle.nn.functional.relu(x=self.bn8(self.linear2(x)))
+        x = paddle.nn.functional.relu(x=self.bn9(self.linear3(x)))
+        x = paddle.nn.functional.relu(x=self.bn10(self.linear4(x)))
+        x = self.final_linear(x)
+        return {self.label_keys[0]: x}
